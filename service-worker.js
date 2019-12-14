b3f8c630ca7b5773b584765349d62449e848239a
otto <otto.lindfors@abo.fi>
2019-12-14 14:53:49 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/csdewas-project-2-ottolindfors/my-react-app/precache-manifest.2abdf58d15a8d45023ee3c62e9aa5ba8.js"
+  "/csdewas-project-2-ottolindfors/precache-manifest.65ad3207a0723646fd1d047da6a31162.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/csdewas-project-2-ottolindfors/my-react-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/csdewas-project-2-ottolindfors/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
